c690bc8b0e2a6b458b4563cba537ab9fd2986658
amirzhou <amirzhou001@gmail.com>
2025-06-11 11:47:41 -0600

    feat: root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
-import { Stack } from "expo-router";
-import { useFonts } from "expo-font";
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loadedFonts] = useFonts({
+    'SFPro-Regular': require('./assets/fonts/SF-Pro-Display-Regular.otf'),
+    'SFPro-Bold': require('./assets/fonts/SF-Pro-Display-Bold.otf'),
+    'SFPro-Text': require('./assets/fonts/SF-Pro-Text-Regular.otf'),
+  });
+
+  useEffect(() => {
+    if (loadedFonts) SplashScreen.hideAsync(); // this is an side effect,  like in a stage ply you don't raise the curtain during rehearsal(render)
+  }, [loadedFonts]);
+
+  if (!loadedFonts) return null;
+
+  return (
+    <Stack>
+      <Stack.Screen name="tabs" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+    </Stack>
+  );
 }
